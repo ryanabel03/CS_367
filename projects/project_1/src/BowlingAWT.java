b0c0a58ea7d65be3fac0447d481f75f21ad996a9
Alex Chau <chaua@mail.gvsu.edu>
2013-01-29 19:00:16 -0500

    Basic frame and junk

---
+++ projects/project_1/src/BowlingAWT.java
@@ -0,0 +1,52 @@
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.GLProfile;
+import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.awt.GLCanvas;
+
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * A minimal program that draws with JOGL in an AWT Frame.
+ *
+ * @author Wade Walker
+ */
+public class BowlingAWT {
+
+    public static void main( String [] args ) {
+        GLProfile glprofile = GLProfile.getDefault();
+        GLCapabilities glcapabilities = new GLCapabilities( glprofile );
+        final GLCanvas glcanvas = new GLCanvas( glcapabilities );
+
+        glcanvas.addGLEventListener( new GLEventListener() {
+            public void reshape( GLAutoDrawable glautodrawable, int x, int y, int width, int height ) {
+                Bowling.setup( glautodrawable.getGL().getGL2(), width, height );
+            }
+
+            public void init( GLAutoDrawable glautodrawable ) {
+            }
+
+            public void dispose( GLAutoDrawable glautodrawable ) {
+            }
+
+            public void display( GLAutoDrawable glautodrawable ) {
+                Bowling.render( glautodrawable.getGL().getGL2(), glautodrawable.getWidth(), glautodrawable.getHeight() );
+            }
+        });
+
+        final Frame frame = new Frame("Bowling");
+        frame.add( glcanvas );
+        frame.addWindowListener( new WindowAdapter() {
+            public void windowClosing( WindowEvent windowevent ) {
+                frame.remove( glcanvas );
+                frame.dispose();
+                System.exit( 0 );
+            }
+        });
+
+        frame.setSize( 800, 600 );
+        frame.setVisible( true );
+    }
+}
